371f3ff5a76e48dbb66003a7cb0868fc0ca2c650
Brandon Varney <b.varney03@gmail.com>
2022-11-17 10:08:20 -0500

    edit activity added

--- src/components/ActivityEdit.js
+++ src/components/ActivityEdit.js
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-const ActivityEdit = () => {
+
+const ActivityEdit = (activity) => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("")
+
+  function changeName (event) {
+    setName(event.target.value)
+  }
+
+  function changeDescription (event) {
+    setDescription(event.target.value)
+  }
+
+  async function formSubmit (event) {
+    event.preventDefault()
+    try {
+      const response = await fetch(`https://fitnesstrac-kr.herokuapp.com/api/activities/${activity.id}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          name: name,
+          description: description
+        })
+      })
+      const data = await response.json();
+      console.log("This is the data: ", data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <div>
-      Edit Activity Form Here:
+      Activity Edit Form:
+      <form onSubmit={formSubmit}>
+        <label>
+          Activity Name:
+        </label>
+        <br />
+        <input onChange={changeName} value={name} type='text'>
+        </input>
+        <br />
+
+        <label>
+          Description:
+        </label>
+        <br />
+        <input onChange={changeDescription} value={description} type='text'>
+        </input>
+        <button type="submit">Update Activity</button>
+        <br />
+        <br />
+
+      </form>
     </div>
   )
 }
