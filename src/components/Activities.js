730e2391e35576d4836a52491ae48dbe03df38f0
Andrew Cook <andrewjcook93@gmail.com>
2022-11-21 02:29:24 -0600

    implemented cosmetic  function gifted by ian

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -4,6 +4,22 @@ import {FcSportsMode} from 'react-icons/fc'
 const Activities = () => {
   const {activitiesObj: [activities, setActivities]} = useOutletContext();
   const reverseActivities = [...activities].reverse();
+
+    //gift to the project by Ser Ian the Glam in exchange for front end assistance
+    function classNameAssigner(){
+        let input = Math.floor(Math.random()*5)
+        if(input>=5) input = input-5;
+        switch(input){
+            case 0: return "zero-color"
+            case 1: return "one-color"
+            case 2: return "two-color"
+            case 3: return "three-color"
+            case 4: return "four-color"
+            default: 
+            return "default-color"
+        }
+        }
+
   return (
     <div>
         <div className="product-title"> <FcSportsMode /> Activities <FcSportsMode /> </div>
@@ -14,7 +30,7 @@ const Activities = () => {
 
             reverseActivities && reverseActivities.length ? reverseActivities.map((activity, idx) => {
 
-                    return <div className="preview-item" key={idx}>
+                    return <div className={classNameAssigner()} key={idx}>
                         
                         <Link to={`/activity/${activity.id}`}><span><b>{activity.name}</b></span></Link>
 
--- src/components/Routines.js
+++ src/components/Routines.js
@@ -2,9 +2,25 @@ import React from 'react';
 import { Link, useOutletContext} from 'react-router-dom';
 import {BiTimer} from 'react-icons/bi'
 
+
 const Routines = () => {
   const {routinesObj: [routines, setRoutines]} = useOutletContext();
   const reverseRoutines = [...routines].reverse();
+  
+  //gift to the project by Ser Ian the Glam in exchange for front end assistance
+  function classNameAssigner(){
+    let input = Math.floor(Math.random()*5)
+    if(input>=5) input = input-5;
+    switch(input){
+      case 0: return "zero-color"
+      case 1: return "one-color"
+      case 2: return "two-color"
+      case 3: return "three-color"
+      case 4: return "four-color"
+      default: 
+        return "default-color"
+    }
+  }
 
   return (
     <div>
@@ -13,7 +29,7 @@ const Routines = () => {
       {
       reverseRoutines && reverseRoutines.length ? reverseRoutines.map((routine, idx) => {
           // console.log(routine)
-        return <div className="preview-item" key = {idx}>
+        return <div className={classNameAssigner()} key = {idx}>
             
             <Link to={`/routines/${routine.id}`}><span><b>{routine.name}</b></span></Link>
 
